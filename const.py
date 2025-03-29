a5753fcfd227dd320709ec7c6efa39a7032b2aa8
amo <mzod73@gmail.com>
2025-03-29 21:42:38 +0300

    enemy movement

--- const.py
+++ const.py
@@ -1,6 +1,5 @@
-
 enemy_kol=0
-enemy_max=10
+enemy_max=1
 interval=1500
 spawn_count = 1
 enemy_size = 40
@@ -9,16 +8,14 @@ enemy_color = (0, 255, 0)
 enemy_hp=100
 bg_x=bg_y=0
 len_move_fone=150
-W = 1280
-H = 960
-FPS = 60
+W = 960
+H = 640
+FPS = 30
 x = W // 2
 y = H // 2
 r = 25
-speed=25
+speed=15
 BLACK = (0, 0, 0)
 GREEN = (0,25,0)
 RED = (255,0,0)
 value=0
-move_r=False
-move_l=False
--- main.py
+++ main.py
@@ -7,28 +7,53 @@ pygame.init()
 from ai_logic import move
 
 player_state = 'default'
+mob_state = 'mob_default'
 move_dict = {
     'right': [pygame.image.load('animations_girl/right/right_1.png'),
               pygame.image.load('animations_girl/right/right_2.png'),
               pygame.image.load('animations_girl/right/right_3.png'),
-              pygame.image.load('animations_girl/right/right_4.png')],
+              pygame.image.load('animations_girl/right/right_4.png')]*2,
 
     'left': [pygame.image.load('animations_girl/left/left_1.png'),
              pygame.image.load('animations_girl/left/left_2.png'),
              pygame.image.load('animations_girl/left/left_3.png'),
-             pygame.image.load('animations_girl/left/left_4.png')],
+             pygame.image.load('animations_girl/left/left_4.png')]*2,
 
     'up': [pygame.image.load('animations_girl/up/up_1.png'),
            pygame.image.load('animations_girl/up/up_2.png'),
            pygame.image.load('animations_girl/up/up_3.png'),
-           pygame.image.load('animations_girl/up/up_4.png')],
+           pygame.image.load('animations_girl/up/up_4.png')]*2,
 
     'down': [pygame.image.load('animations_girl/down/down_1.png'),
              pygame.image.load('animations_girl/down/down_2.png'),
              pygame.image.load('animations_girl/down/down_3.png'),
-             pygame.image.load('animations_girl/down/down_4.png')],
+             pygame.image.load('animations_girl/down/down_4.png')]*2,
 
-    'default': [pygame.image.load('animations_girl/down/down_1.png')] * 4
+    'default': [pygame.image.load('animations_girl/down/down_1.png')] * 8,
+
+
+    'mob_attak_down': [pygame.image.load('animations_mob/attak/orc1_attack_down.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_down1.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_down2.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_down3.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_down4.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_down5.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_down6.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_down7.png')],
+
+
+    'mob_default': [pygame.image.load('animations_mob/attak/orc1_attack_down.png')]*8,
+
+    'mob_attak_down_up': [pygame.image.load('animations_mob/attak/orc1_attack_up.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_up1.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_up2.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_up3.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_up4.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_up5.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_up6.png'),
+                  pygame.image.load('animations_mob/attak/orc1_attack_up7.png')]
+
+
 }
 
 enemies = []
@@ -57,7 +82,7 @@ def spawn_enemies(count=1):
     ]
 
 
-font = pygame.font.Font('alagard-12px-unicode.ttf', 26)
+font = pygame.font.Font('alagard-12px-unicode.ttf', 20)
 
 screen = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
@@ -82,31 +107,36 @@ while running:
     if keys[pygame.K_w]:
         y -= speed
         player_state = 'up'
+        player_move=True
     elif keys[pygame.K_s]:
         y += speed
         player_state = 'down'
+        player_move = True
     elif keys[pygame.K_a]:
         x -= speed
         player_state = 'left'
+        player_move = True
     elif keys[pygame.K_d]:
         x += speed
         player_state = 'right'
+        player_move = True
     else:
         player_state = 'default'
+        player_move = False
+
 
     if event.type == pygame.MOUSEBUTTONDOWN:
         if event.button == 1:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             mouse_rect = Rect(mouse_x, mouse_y, 40, 40)
             for enemy in enemies:
-                enm_rect = Rect((enemy['x'], enemy['y'], enemy['width'], enemy['height']))
+                enm_rect = Rect((enemy['x'], enemy['y'], 128,128))
                 if Rect.colliderect(mouse_rect, enm_rect):
                     dmg = random.randint(15, 25)
                     if enemy['hp'] - dmg > 1:
                         enemy['hp'] -= dmg
                         enemy['damage'] = dmg
                     else:
-
                         enemies.remove(enemy)
 
     if current_time - last_time > interval:
@@ -118,76 +148,69 @@ while running:
                 enemy_kol += 1
 
     for i, enemy in enumerate(enemies):
-        if rasst(enemy['x'], enemy['y'], x, y) > r:#убрать
-            temp_x, temp_y = move(enemy['x'], enemy['y'], x, y, 5)
-            for j, other_enemy in enumerate(enemies):
-                if (other_enemy is not enemy
-                        and other_enemy['x'] <= temp_x <= other_enemy['x'] + other_enemy["width"]
-                        and other_enemy['y'] <= temp_y <= other_enemy['y'] + other_enemy["height"]):
-                    break
-                # if pygame.Rect(enemy['x']).colliderect(pygame.Rect()):
-                #     break
+        if rasst(enemy['x']//2, enemy['y']//2, x//2, y//2) > 48:
+            enemy['x'], enemy['y'] = move(enemy['x'], enemy['y'], x, y, 5)
+            mob_state = 'mob_default'
+        else:
+            if enemy['y']<y and enemy['x']<x:
+                mob_state='mob_attak_down'
             else:
-                enemy['x'] = temp_x
-                enemy['y'] = temp_y
+                mob_state = 'mob_attak_down_up'
+        # for j, other_enemy in enumerate(enemies):
+        #     if Rect.colliderect(Rect(temp_x,temp_y,enemy['height'],enemy['width']),
+        #                           Rect(enemy['x'],enemy['y'],enemy['height'],enemy['width'])):
+        #         break
+        #             # if (other_enemy is not enemy
+        #             #         and other_enemy['x'] <= temp_x <= other_enemy['x'] + other_enemy["width"]
+        #             #         and other_enemy['y'] <= temp_y <= other_enemy['y'] + other_enemy["height"]):
+        #             #     break
+        #
+        # else:
+        #     enemy['x'] = temp_x
+        #     enemy['y'] = temp_y
 
-    if x - r <= 0:
-        x = r
-    elif x + r >= W:
-        x = W - r
-    if y - r <= 0:
-        y = r
-    elif y + r >= H:
-        y = H - r
+    if x <= 0:
+        x = 0
+    elif x + 64 >= W:
+        x = W - 64
+    if y  <= 0:
+        y = 0
+    elif y + 64 >= H:
+        y = H - 64
 
-    if x - r < len_move_fone:
 
-        bg_x = min(bg_x + speed, 0)
-    elif x + r > W - len_move_fone:
-        bg_x = max(bg_x - speed, -(background_rect.width - W))
-    if y - r < len_move_fone:
-        bg_y = min(bg_y + speed, 0)
-    elif y + r > H - len_move_fone:  #
-        bg_y = max(bg_y - speed, -(background_rect.height - H))
+    if player_move:
+        if x < len_move_fone:
+            bg_x = min(bg_x + speed, 0)
+        elif x  > W - len_move_fone:
+            bg_x = max(bg_x - speed, -(background_rect.width - W))
+        if y < len_move_fone:
+            bg_y = min(bg_y + speed, 0)
+        elif y > H - len_move_fone:  #
+            bg_y = max(bg_y - speed, -(background_rect.height - H))
 
-    # if move_l:
-    #     value_l+=1
-    # else:
-    #     if value_l%2!=0:
-    #         value_l+=1
-    # if value_l>=4:
-    #     value_l=0
-    # image_l = girl_move_l[value_l]
-    # image_l = pygame.transform.scale(image_l, (60, 76))
+    image = move_dict[player_state][pygame.time.get_ticks() % 8]
+    image = pygame.transform.scale(image, (64, 96))
 
-    # if move_r:
-    #     value+=1
-    # else:
-    #     if value%2!=0:
-    #         value+=1
-    # if value>=4:
-    #     value=0
-    image = move_dict[player_state][pygame.time.get_ticks() % 4]
-    image = pygame.transform.scale(image, (64, 64))
-
+    image_mob_attak = move_dict[mob_state][pygame.time.get_ticks() % 8]
+    image_mob_attak = pygame.transform.scale(image_mob_attak, (128, 128))
     screen.blit(background_image, (bg_x, bg_y))
-    # pygame.draw.circle(screen, (255, 0, 0), (x, y), r)
-    # pygame.draw.rect(screen, (0, 255, 0), rect1)
-    # pygame.draw.rect(screen, (0, 0, 255), bullet)
+
 
     for enemy in enemies:
-        pygame.draw.rect(
-            screen,
-            enemy['color'],
-            (enemy['x'], enemy['y'], enemy['width'], enemy['height'])
-        )
-        text = font.render(f'{enemy['hp']}/100', True, RED)
+        screen.blit(image_mob_attak, (enemy['x'], enemy['y']))
+        # pygame.draw.rect(
+        #     screen,
+        #     enemy['color'],
+        #     (enemy['x'], enemy['y'], enemy['width'], enemy['height'])
+        # )
+        text = font.render(f'{enemy['hp']}/100hp', True, RED)
         textRect = text.get_rect()
-        textRect.center = (enemy['x'] + enemy_size // 2, enemy['y'] - 20)
+        textRect.center = (enemy['x'] +82, enemy['y']-30)
         screen.blit(text, textRect)
 
     screen.blit(image, (x, y))
-    # screen.blit(image_l, (x, y))
+
 
     pygame.display.update()
     clock.tick(FPS)
